9ae7e735eafc4adc9fb6b23172208120f34cc340
a-ramji <anna@ramji.org>
2023-08-25 10:53:09 -0700

    Initial commit

---
+++ R/theme_eighties.R
@@ -0,0 +1,9 @@
+theme_eighties <- function() {
+  theme(plot.background = element_rect(fill = "magenta"),
+        panel.background = element_rect(fill = "grey"),
+        axis.text  = element_text(color = "yellow"),
+        panel.grid = element_line(color = "cyan")
+
+        )
+}
+
